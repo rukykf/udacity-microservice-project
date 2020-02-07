15a282712b5cb5ccbcfe9a3bda90f2d0a9fe6fa3
vagrant <vagrant@localhost.localdomain>
2020-02-07 02:55:05 +0000

    completed project instructions

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=rukkykf/predictions-image
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag predictions-image $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
